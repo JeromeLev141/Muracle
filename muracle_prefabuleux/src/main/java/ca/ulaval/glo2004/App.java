664c8a3d081e4789e9c4a822a2d2c295d318aef4
Anthony Deschênes <anthony.deschenes.1@ulaval.ca>
2022-09-21 09:24:42 -0400

    "initial commit"

---
+++ muracle_prefabuleux/src/main/java/ca/ulaval/glo2004/App.java
@@ -0,0 +1,22 @@
+package ca.ulaval.glo2004;
+
+
+import ca.ulaval.glo2004.gui.MainWindow;
+
+import javax.swing.*;
+
+
+public class App {
+    //Exemple de creation d'une fenetre et d'un bouton avec swing. Lorsque vous allez creer votre propre GUI
+    // Vous n'aurez pas besoin d'ecrire tout ce code, il sera genere automatiquement par intellij ou netbeans
+    // Par contre vous aurez a creer les actions listener pour vos boutons et etc.
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("MainWindow");
+        frame.setContentPane(new MainWindow().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+}
+
