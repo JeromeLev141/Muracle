75a71596c5aad36695ce65581198e32aef8bc77b
Jérôme <jerome.levesque.14@ulaval.ca>
2022-10-13 17:46:33 -0400

    Ajout de la demo du gui

--- muracle_prefabuleux/src/main/java/ca/ulaval/glo2004/App.java
+++ muracle_prefabuleux/src/main/java/ca/ulaval/glo2004/App.java
@@ -7,16 +7,15 @@ import javax.swing.*;
 
 
 public class App {
-    //Exemple de creation d'une fenetre et d'un bouton avec swing. Lorsque vous allez creer votre propre GUI
-    // Vous n'aurez pas besoin d'ecrire tout ce code, il sera genere automatiquement par intellij ou netbeans
-    // Par contre vous aurez a creer les actions listener pour vos boutons et etc.
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("MainWindow");
-        frame.setContentPane(new MainWindow().panel1);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
 
+    public static void main(String[] args) {
+        MainWindow mainWindow = new MainWindow();
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            SwingUtilities.updateComponentTreeUI(mainWindow);
+        }catch (Exception e){}
+        mainWindow.setVisible(true);
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
 
