400dd014da12ad00beeeaa06af644c3d2c6587b8
Alex Prud'homme <alex.prudhomme.1@ulaval.ca>
2022-11-11 15:40:00 -0500

    addAccessoire()
    addAccessoire qui check si accessoire fit dans cote et si il intersecte avec d'autres accessoires

--- muracle_prefabuleux/src/main/java/muracle/domaine/Cote.java
+++ muracle_prefabuleux/src/main/java/muracle/domaine/Cote.java
@@ -7,6 +7,7 @@ import muracle.utilitaire.PouceError;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Objects;
 
 public class Cote implements java.io.Serializable{
     private char orientation;
@@ -14,7 +15,7 @@ public class Cote implements java.io.Serializable{
     private Pouce hauteur;
     private ArrayList<Mur> murs;
     private ArrayList<Pouce> separateurs;
-    private Accessoire[] accessoires;
+    private ArrayList<Accessoire> accessoires;
 
     public Cote(){
 
@@ -25,6 +26,7 @@ public class Cote implements java.io.Serializable{
         this.hauteur = hauteur;
         murs = new ArrayList<>();
         separateurs = new ArrayList<>();
+        accessoires = new ArrayList<>();
     }
 
     public char getOrientation() {
@@ -47,25 +49,70 @@ public class Cote implements java.io.Serializable{
         return hauteur;
     }
     public Accessoire getAccessoire(int index){
-        return this.accessoires[index];
+        return accessoires.get(index);
     }
 
-    public Accessoire[] getAccessoires() {
+    public ArrayList<Accessoire> getAccessoires() {
         return accessoires;
     }
 
-    public void addAccessoire(Accessoire accessoire, CoordPouce positionInit){
+    //TO REMOVE
+    public void setAccessoires(ArrayList<Accessoire> accessoires){
+        this.accessoires = accessoires;
+    }
 
+    public void addAccessoire(Accessoire accessoire) throws FractionError, PouceError {
+        if(doesAccessoireFitInCote(accessoire) && doesAccessoireFitWithOtherAccessoires(accessoire)){
+            accessoires.add(accessoire);
+        }
+    }
+    public boolean doesAccessoireFitInCote(Accessoire accessoire) throws FractionError, PouceError {
+        CoordPouce cote1 = new CoordPouce(new Pouce("0"), new Pouce("0"));
+        CoordPouce cote2 = new CoordPouce(largeur, hauteur);
+        CoordPouce accessoire1 = accessoire.getPosition();
+        CoordPouce accessoire2 =new CoordPouce(accessoire.getPosition().getX().add(accessoire.getLargeur()), accessoire.getPosition().getY().add(accessoire.getHauteur()));
+
+        boolean isLeftValid = (cote1.getX().compare(accessoire1.getX()) == -1) || (cote1.getX().compare(accessoire1.getX()) == 0);
+        boolean isRightValid = (cote2.getX().compare(accessoire2.getX()) == 1) ||  (cote2.getX().compare(accessoire2.getX()) == 0);
+        boolean isTopValid = (cote1.getY().compare(accessoire1.getY()) == -1) || (cote1.getY().compare(accessoire1.getY()) == 0);
+        boolean isBotValid = (cote2.getY().compare(accessoire2.getY()) == 1) || (cote2.getY().compare(accessoire2.getY()) == 0);
+
+        return isRightValid && isLeftValid && isBotValid && isTopValid;
+    }
+
+    // Suit cet algorithme
+    // https://silentmatt.com/rectangle-intersection/
+    public boolean doesAccessoireFitWithOtherAccessoires(Accessoire accessoire) throws FractionError {
+        CoordPouce mainAccessoire1 = accessoire.getPosition();
+        CoordPouce mainAccessoire2 = new CoordPouce(accessoire.getPosition().getX().add(accessoire.getLargeur()), accessoire.getPosition().getY().add(accessoire.getHauteur()));
+        if(!accessoires.isEmpty()){
+            for(int i = 0; i < accessoires.size(); i++){
+                CoordPouce secondAccessoire1 = getAccessoire(i).getPosition();
+                CoordPouce secondAccessoire2 = new CoordPouce(getAccessoire(i).getPosition().getX().add(getAccessoire(i).getLargeur()), getAccessoire(i).getPosition().getY().add(getAccessoire(i).getHauteur()));
+
+                if((mainAccessoire1.getX().compare(secondAccessoire2.getX()) == -1) &&
+                        mainAccessoire2.getX().compare(secondAccessoire1.getX()) == 1 &&
+                        mainAccessoire1.getY().compare(secondAccessoire2.getY()) == -1 &&
+                        mainAccessoire2.getY().compare(secondAccessoire1.getY()) == 1 ){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
     public void moveAccessoire(CoordPouce positionInit, CoordPouce positionPost){
-        //TODO
+        //check if position valide
     }
     public void removeAccessoire(CoordPouce position){
         //TODO
     }
     private boolean ContainRetourAir(){
-        //TODO
-        return true;
+        for (Accessoire accessoire : accessoires) {
+            if (Objects.equals(accessoire.getType(), "RetourAir")) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public void setHauteur(Pouce hauteur) {
