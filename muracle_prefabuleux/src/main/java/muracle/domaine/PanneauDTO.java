cb0497332ed8620d41e989592cfd59f41344a02a
Max <maxime.bouchard.14@ulaval.ca>
2022-11-17 16:45:59 -0500

    Implementation DTO Panneau et panneau

--- muracle_prefabuleux/src/main/java/muracle/domaine/PanneauDTO.java
+++ muracle_prefabuleux/src/main/java/muracle/domaine/PanneauDTO.java
@@ -1,4 +1,20 @@
 package muracle.domaine;
 
+import muracle.utilitaire.Pouce;
+
+import java.util.UUID;
+
 public class PanneauDTO {
+
+    private Pouce Largeur;
+    private Pouce Hauteur;
+    private double  Poids;
+    public UUID id;
+
+    public PanneauDTO(Panneau panneau){
+        Largeur = panneau.getLargeur();
+        Hauteur = panneau.getHauteur();
+        Poids = panneau.getPoids();
+    }
+
 }
