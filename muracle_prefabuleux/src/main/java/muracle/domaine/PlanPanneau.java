835d8c614897589e3e49b4c38a5ff46284398148
jeremi_girard <girje1932987@etu.cegepjonquiere.ca>
2022-12-12 19:41:00 -0500

    correction des bug de GenerateurPlan puis ajout d'annotation du code

--- muracle_prefabuleux/src/main/java/muracle/domaine/PlanPanneau.java
+++ muracle_prefabuleux/src/main/java/muracle/domaine/PlanPanneau.java
@@ -1,8 +1,10 @@
 package muracle.domaine;
 
 import muracle.utilitaire.CoordPouce;
+import muracle.utilitaire.Pouce;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class PlanPanneau {
@@ -11,37 +13,80 @@ public class PlanPanneau {
     private List<List<CoordPouce>> polygoneAccessoire;
 
 
-
+    /**
+     * @biref constructeur de la class PlanPanneau
+     */
     public PlanPanneau(){
         this.polygone = new ArrayList<>();
         this.lignePlie = new ArrayList<>();
         this.polygoneAccessoire = new ArrayList<>();
     }
 
+    /**
+     * @brief ajout un points dans la liste polygone
+     * @param coordPouce: coordonnée du point
+     */
     public void ajoutPointPolygone(CoordPouce coordPouce){
         polygone.add(coordPouce);
     }
 
+    /**
+     * @brief ajout un tableau de 2 coordPouce qui représente une ligne de plie
+     * @param coordPouces
+     */
     public void ajoutlignePlie(CoordPouce[] coordPouces){
         lignePlie.add(coordPouces);
     }
 
+    /**
+     * @brief ajout une list de CoordPouce qui représente un accessoir
+     * @param accessoire: list<CoordPouce>
+     */
     public void ajoutAccessoire(List<CoordPouce> accessoire){
         System.out.println(accessoire);
         polygoneAccessoire.add(accessoire);
     }
 
+    /**
+     * @brief getteur de l'attribut polygone
+     * @return polygone: List<CoordPouce>
+     */
     public List<CoordPouce> getPolygone(){
         return polygone;
     }
 
+    /**
+     * @brief getteur de l'attribut lignePlie
+     * @return polygone: List<CoordPouce[]>
+     */
     public List<CoordPouce[]> getLignePlie(){
         return lignePlie;
     }
 
+    /**
+     * @brief getteur de l'attribut polygoneAccessoire
+     * @return polygone: List<List<CoordPouce>>
+     */
     public List<List<CoordPouce>> getPolygoneAccessoire(){
         return polygoneAccessoire;
     }
+
+    /**
+     * @brief retourne la largeur du plan
+     * @return Pouce
+     */
+    public Pouce getLargeur(){
+        return polygone.stream().max(Comparator.comparing(i -> i.getX().toDouble())).get().getX();
+    }
+
+    /**
+     * @brief retourne la hauteur du plan
+     * @return Pouce
+     */
+    public Pouce getHauteur(){
+        return polygone.stream().max(Comparator.comparing(i -> i.getY().toDouble())).get().getY();
+    }
+
 
     @Override
     public String toString(){
--- muracle_prefabuleux/src/main/java/muracle/utilitaire/CoordPouce.java
+++ muracle_prefabuleux/src/main/java/muracle/utilitaire/CoordPouce.java
@@ -5,33 +5,64 @@ public class CoordPouce implements java.io.Serializable{
     private Pouce x;
     private Pouce y;
 
+    /**
+     * @brief constructeur de la classe CoordPouce
+     * @param x: coordonné en X
+     * @param y: coordonné en Y
+     */
     public CoordPouce(Pouce x,Pouce y){
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * @brief getteur de l'attribut X
+     * @return Pouce
+     */
     public Pouce getX() {
         return x;
     }
 
+    /**
+     * @brief setteur de l'attribut X
+     * @param x: Pouce
+     */
     public void setX(Pouce x) {
         this.x = x;
     }
 
+    /**
+     * @brief getteur de l'attribut Y
+     * @return Pouce
+     */
     public Pouce getY() {
         return y;
     }
 
+    /**
+     * @brief setteur de l'attribut Y
+     * @param y:Pouce
+     */
     public void setY(Pouce y) {
         this.y = y;
     }
 
+    /**
+     * @brief arroudie X et Y à value
+     * @param value (int) : arroudie a cette valeur
+     * @return CoordPouce
+     * @throws FractionError: erreur si la valeur passée en paramètre est invalide
+     */
     public CoordPouce round(int value) throws FractionError {
         x.round(value);
         y.round(value);
         return this;
     }
 
+    /**
+     * @brief génère une copy en profondeur de l'objet
+     * @return CoordPouce
+     */
     public CoordPouce copy(){
         return new CoordPouce(this.x.copy(),this.y.copy());
     }
