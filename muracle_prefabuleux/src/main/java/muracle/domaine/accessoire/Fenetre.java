45db774462bad6d07d5b796aee8921c571c51557
Max <maxime.bouchard.14@ulaval.ca>
2022-12-17 13:39:02 -0500

    Commentaire Fenetre

--- muracle_prefabuleux/src/main/java/muracle/domaine/accessoire/Fenetre.java
+++ muracle_prefabuleux/src/main/java/muracle/domaine/accessoire/Fenetre.java
@@ -5,12 +5,19 @@ import muracle.utilitaire.CoordPouce;
 import muracle.utilitaire.Pouce;
 
 public class Fenetre extends Accessoire {
-
+    /**
+     * @brief constructeur de fenetre vide
+     */
     public Fenetre(){
         this.setType("Fenêtre");
         this.setInterieurOnly(Boolean.FALSE);
     }
-
+    /**
+     * @brief constructeur de Fenetre, avec comme type Fenêtre et l'attribue interieur seulement à faux
+     * @param largeur:Pouce
+     * @param hauteur:Pouce
+     * @param position:CoordPouce
+     */
     public Fenetre(Pouce largeur, Pouce hauteur, CoordPouce position){
 
         this.setType("Fenêtre");
