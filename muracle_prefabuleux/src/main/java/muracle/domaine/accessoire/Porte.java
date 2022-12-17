9b77e7867c1ffee00a1ca515dcfabb290d47b0a0
Max <maxime.bouchard.14@ulaval.ca>
2022-12-17 13:40:01 -0500

    Commentaire Porte

--- muracle_prefabuleux/src/main/java/muracle/domaine/accessoire/Porte.java
+++ muracle_prefabuleux/src/main/java/muracle/domaine/accessoire/Porte.java
@@ -5,12 +5,19 @@ import muracle.utilitaire.CoordPouce;
 import muracle.utilitaire.Pouce;
 
 public class Porte extends Accessoire {
-
+    /**
+     * @brief constructeur de Porte vide
+     */
     public Porte(){
         this.setType("Porte");
         this.setInterieurOnly(Boolean.FALSE);
     }
-
+    /**
+     * @brief constructeur de Porte, avec comme type Porte et l'attribue interieur seulement à faux
+     * @param largeur:Pouce
+     * @param hauteur:Pouce
+     * @param position:CoordPouce
+     */
     public Porte(Pouce largeur, Pouce hauteur, CoordPouce position){
         this.setType("Porte");
         this.setInterieurOnly(Boolean.FALSE);
