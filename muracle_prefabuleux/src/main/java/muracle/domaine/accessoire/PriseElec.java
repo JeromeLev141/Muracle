08e8d440bf3715e7c0eaa5ac8ed0bd1742b7e7b1
Max <maxime.bouchard.14@ulaval.ca>
2022-12-17 13:41:26 -0500

    Commentaire Prise Elec

--- muracle_prefabuleux/src/main/java/muracle/domaine/accessoire/PriseElec.java
+++ muracle_prefabuleux/src/main/java/muracle/domaine/accessoire/PriseElec.java
@@ -5,12 +5,19 @@ import muracle.utilitaire.CoordPouce;
 import muracle.utilitaire.Pouce;
 
 public class PriseElec extends Accessoire {
-
+    /**
+     * @brief constructeur de PriseElec vide
+     */
     public PriseElec(){
         this.setType("Prise électrique");
         this.setInterieurOnly(Boolean.TRUE);
     }
-
+    /**
+     * @brief constructeur de PriseElec, avec comme type Prise électrique et l'attribue interieur seulement à faux
+     * @param largeur:Pouce
+     * @param hauteur:Pouce
+     * @param position:CoordPouce
+     */
     public PriseElec(Pouce largeur, Pouce hauteur, CoordPouce position){
         this.setType("Prise électrique");
         this.setInterieurOnly(Boolean.TRUE);
